71ffca464111246ea17a027aa0b9a3cb7a59f729
Lukenrs <lukenbr@gmail.com>
2022-02-09 15:05:24 -0300

    correção

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, {Component} from "react"
 
 
 export default class App extends Component{
-  mens = (x) =>{
-    return "O dobro do número escolhido foi:"
+  mens = (m) =>{
+    return m
   }
   num = (x)=>{
     return x*2
   }
   render(){
     return(
-      <div><h1>{this.mens()} {this.num(2)}</h1></div>
+      <div><h1>{this.mens("O dobro do valor escolhido é:")} {this.num(2)}</h1></div>
     )
   }
 }
